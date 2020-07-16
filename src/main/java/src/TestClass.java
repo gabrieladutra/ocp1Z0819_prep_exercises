cc4dc148dbf93ee19ab2eecd91d8d3d10aecf1c2
gabrieladutra <gabriela_ddutra@outlook.com>
2020-07-15 21:22:51 -0300

    Added the answer to question 3

--- src/main/java/src/TestClass.java
+++ src/main/java/src/TestClass.java
@@ -1,18 +1,24 @@
 /*
- 4.7 Exercise - Question 2
+ 4.7 Exercise - Question 3
  */
 package src;
 
 public class TestClass {
     static Bird bird = new Bird();
 
+
     public static void main(String[] args) {
-        System.out.println(bird.age);
+        Bird secondBird = new Bird();
+        secondBird.name = "Woodpecker";
+        secondBird.age = 2;
+
+        bird.name = secondBird.name;
+        bird.age = secondBird.age;
+
         System.out.println(bird.name);
-        System.out.println(Bird.height);
-        System.out.println(Bird.isSinger);
-        System.out.println(Bird.weight);
-        System.out.println(TestClass.bird);
+        System.out.println(bird.age);
+        System.out.println(secondBird.name);
+        System.out.println(secondBird.age);
 
     }
 }

c3eddfe8fd3bd435dd28e4857dbb396877166772
gabrieladutra <gabriela_ddutra@outlook.com>
2020-07-15 21:09:23 -0300

    Added the answer to question 1

---
+++ src/main/java/src/Bird.java
@@ -0,0 +1,17 @@
+/*
+ 4.7 Exercise - Question 1
+ */
+package src;
+
+public class Bird {
+    String name;
+    int age;
+    static float height;
+    static double weight;
+    static boolean isSinger;
+
+    public void fly(){
+
+    }
+
+}

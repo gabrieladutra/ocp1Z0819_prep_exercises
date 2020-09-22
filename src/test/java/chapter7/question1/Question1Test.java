834a8c96debe60a79f7357f9f29c9f4e521cd93d
Gabriela DIas Dutra <gabriela_ddutra@outlook>
2020-09-22 15:52:35 -0300

    Deleting files.

---
+++ src/test/java/chapter7/question1/Question1Test.java
@@ -0,0 +1,31 @@
+package chapter7.question1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import src.chapter7.question1.Question1;
+
+public class Question1Test {
+    @Test
+    public void isEven_whenANumberIsEven_shouldReturnTrue() {
+
+        assertTrue(Question1.isEven(10));
+    }
+
+    @Test
+    public void isEven_whenANumberIsNotEven_shouldReturnFalse() {
+        assertFalse(Question1.isEven(11));
+    }
+
+    @Test
+    public void isEvenTernary_whenNumberIsEven_shouldReturnTrue() {
+        assertTrue(Question1.isEvenTernary(10));
+    }
+
+    @Test
+    public void isEvenTernary_whenNumberIsNotEven_shouldReturnFalse() {
+        assertFalse(Question1.isEvenTernary(11));
+    }
+
+}

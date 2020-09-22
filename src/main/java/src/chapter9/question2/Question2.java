3cd4a85c23e992d5841da1da75ef25dd8e9b4ce5
Gabriela DIas Dutra <gabriela_ddutra@outlook>
2020-09-22 15:43:27 -0300

    FIxing method name.

--- src/main/java/src/chapter9/question2/Question2.java
+++ src/main/java/src/chapter9/question2/Question2.java
@@ -7,7 +7,7 @@ public class Question2 {
         if(paramArray == null) throw new IllegalArgumentException("Array is null");
         return paramArray.length;
     }
-    public static void printArrayElement(int[] paramArray){
+    public static void printArrayElements(int[] paramArray){
         for(int i : paramArray) System.out.println(i);
         //Arrays.stream(paramArray).forEach(elementoAtual -> System.out.println(elementoAtual));
         //Arrays.stream(paramArray).forEach(System.out::println);
